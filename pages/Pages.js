ae0bb361c31ec1f5b7fedb0922450fb2e52f7596
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-08-12 18:40:21 -0700

    Provide cache headers to things which dont change (even when debugging)

--- pages/Pages.js
+++ pages/Pages.js
@@ -12,7 +12,6 @@ const JSPages = {
   '/js/script.js':      `${__dirname}/script.js`,
   '/js/jquery.js':      `${__dirname}/../node_modules/jquery/dist/jquery.js`,
   '/js/bootstrap.js':   `${__dirname}/../node_modules/bootstrap/dist/js/bootstrap.bundle.js`,
-  '/js/plotly.js':      `${__dirname}/../node_modules/plotly.js-dist/plotly.js`,
   '/js/sortable.js':    `${__dirname}/../node_modules/sortablejs/Sortable.min.js`,
   '/js/dx.all.js':      `${__dirname}/../node_modules/devextreme/dist/js/dx.all.js`
 };
@@ -29,37 +28,37 @@ function Register(root, wsroot) {
 
   for (let key in JSPages) {
     const file = JSPages[key];
-    if (DEBUG) {
-      JSPages[key] = () => FS.readFileSync(file, { encoding: 'utf8' });
+    if (DEBUG && file.indexOf('node_modules') === -1) {
+      JSPages[key] = () => { return { body: FS.readFileSync(file, { encoding: 'utf8' }) } };
     }
     else {
       const contents = FS.readFileSync(file, { encoding: 'utf8' });
-      JSPages[key] = () => contents;
+      JSPages[key] = () => { return { body: contents, cacheControl: { maxAge: CACHE_MAXAGE } } };
     }
     root.get(key, async ctx => {
-      ctx.body = JSPages[key]();
-      ctx.type = 'text/javascript';
-      if (!DEBUG) {
-        ctx.cacheControl = { maxAge: CACHE_MAXAGE };
+      const result = JSPages[key]();
+      for (let key in result) {
+        ctx[key] = result[key];
       }
+      ctx.type = 'text/javascript';
     });
   }
 
   for (let key in CSSPages) {
     const file = CSSPages[key];
-    if (DEBUG) {
-      CSSPages[key] = () => FS.readFileSync(file, { encoding: 'utf8' });
+    if (DEBUG && file.indexOf('node_modules') === -1) {
+      CSSPages[key] = () => { return { body: FS.readFileSync(file, { encoding: 'utf8' }) } };
     }
     else {
       const contents = FS.readFileSync(file, { encoding: 'utf8' });
-      CSSPages[key] = () => contents;
+      CSSPages[key] = () => { return { body: contents, cacheControl: { maxAge: CACHE_MAXAGE } } };
     }
-    root.get(key, async (ctx) => {
-      ctx.body = CSSPages[key]();
-      ctx.type = 'text/css';
-      if (!DEBUG) {
-        ctx.cacheControl = { maxAge: CACHE_MAXAGE };
+    root.get(key, async ctx => {
+      const result = CSSPages[key]();
+      for (let key in result) {
+        ctx[key] = result[key];
       }
+      ctx.type = 'text/css';
     });
   }
 
