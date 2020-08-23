a19408b0b0bef834f5efaf38a59b7d5cd530cff4
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-08-22 18:46:19 -0700

    Set name using oid

--- device/switch/trendnet/teg-30284/write/name.js
+++ device/switch/trendnet/teg-30284/write/name.js
@@ -1,31 +1,13 @@
+const OID = require('../../../../../OID');
+
 module.exports = {
   system: {
     $: 'guard',
     key: 'system.name,system.location,system.contact',
     arg: {
-      $: 'fetch',
-      arg: '/iss/specific/rpc.js',
-      frame: 'myframe',
-      method: 'post',
-      params: {
-        Gambit: {
-          $: 'eval',
-          arg: 'GetInputGambit()'
-        },
-        RPC: {
-          $: 'tojson',
-          arg: {
-            method: 'CommonSet',
-            id: 0,
-            params: {
-              Template: 'sysSystemInfo',
-              sysInfoSysName: { $: 'kv', arg: 'system.name' },
-              sysInfoSysLocation: { $: 'kv', arg: 'system.location' },
-              sysInfoSysContact: { $: 'kv', arg: 'system.contact' }
-            }
-          }
-        }
-      }
+      name: { $: 'oid+set', arg: OID.system.name },
+      location: { $: 'oid+set', arg: OID.system.location },
+      contact: { $: 'oid+set', arg: OID.system.contact },
     }
   }
 };
