32f1737fec0520f8a04e619a2b8ed7baaa595abd
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-09-20 15:42:40 -0700

    Capture skeleton page

--- pages/Capture/index.js
+++ pages/Capture/index.js
@@ -1,20 +1,30 @@
 const Template = require('../Template');
 const Page = require('../Page');
+const DeviceInstanceManager = require('../../DeviceInstanceManager');
+const TopologyManager = require('../../TopologyManager');
 
 class Capture extends Page {
 
   constructor(send) {
     super(send);
     this.state = {
+      devices: [],
+      topologyValid: false
     };
   }
 
   select() {
     super.select();
+    this.updateState();
     this.html('main-container', Template.CaptureTab(this.state));
   }
 
   deselect() {
+  }
+
+  updateState() {
+    this.state.devices = DeviceInstanceManager.getCaptureDevices();
+    this.state.topologyValid = TopologyManager.valid;
   }
 
 }
