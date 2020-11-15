61f4850861bc7748a61501c78459d31fbefbb6fa
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-11-14 23:44:38 -0800

    Improve wifi display

--- pages/WiFi/index.js
+++ pages/WiFi/index.js
@@ -34,17 +34,21 @@ class WiFi extends Page {
     if (this.state.selectedIdx === -1) {
       this.state.selectedIdx = 0;
     }
-    this.state.selected = this.state.station[this.state.selectedIdx];
-    if (this.state.selected) {
-      this.state.selected.instances.forEach(instance => {
+    const station = this.state.station[this.state.selectedIdx];
+    if (station) {
+      station.instances.forEach(instance => {
         this.state.ports[this.state.devices.indexOf(instance.device)] = { active: true };
       });
+      this.state.selected = WiFiManager.getStationConfig(station);
+    }
+    else {
+      this.state.selected = null;
     }
   }
 
   async 'select.ssid' (msg) {
     const ssid = msg.value;
-    const idx = this.state.station.findIndex(station => station.ssid.name == ssid);
+    const idx = this.state.station.findIndex(station => station.instances[0].station.ssid == ssid);
     if (idx === -1) {
       return;
     }
@@ -56,12 +60,13 @@ class WiFi extends Page {
   async 'device.toggle' (msg) {
     const device = this.state.devices[msg.value];
     if (this.state.ports[msg.value]) {
-      this.state.ports[msg.value] = null;
+      WiFiManager.removeDeviceFromStation(this.state.selected.station, device);
     }
     else {
-      this.state.ports[msg.value] = { active: true };
+      WiFiManager.addDeviceToStation(this.state.selected.station, device);
     }
-    this.html('wifi-devices', Template.PortsDevices(this.state));
+    this.updateState();
+    this.html('wifi-selected', Template.WiFiSelected(this.state));
   }
 
   async 'network.wifi.create' (msg) {
@@ -70,15 +75,14 @@ class WiFi extends Page {
     if (idx !== -1) {
       // Already exists - so just select it
       this.state.selectedIdx = idx;
-      this.updateState();
     }
     else {
       WiFiManager.createStation(ssid);
       this.state.station = WiFiManager.getAllStations();
       this.state.selectedIdx = this.state.station.findIndex(station => station.ssid.name == ssid);
-      this.updateState();
-      this.html('wifi-list', Template.WiFiList(this.state));
     }
+    this.updateState();
+    this.html('wifi-list', Template.WiFiList(this.state));
     this.html('wifi-selected', Template.WiFiSelected(this.state));
   }
 }
