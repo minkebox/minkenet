50c75f139cac161eea17f23452c8eb2ccdcfb7fd
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-12-07 22:20:32 -0800

    TL-SG105PE

---
+++ device/switch/tp-link/tl-sg105pe/index.js
@@ -0,0 +1,83 @@
+const Device = {
+
+  name: 'TL-SG105PE',
+  id: 'tplink/tlsg105pe',
+  image: require('./image'),
+
+  layout: {
+    ports: [
+      [ 0, 1, 2, 3, 4 ]
+    ]
+  },
+
+  properties: {
+    switch: true
+  },
+
+  identify: {
+    http: {
+      ipv4: 'dhcp',
+      loggedIn: {
+        $: 'eval',
+        arg: `top.g_title === 'TL-SG105PE'`
+      }
+    },
+    escp: {
+      txt: {
+        model: 'TL-SG105PE'
+      }
+    }
+  },
+
+  login: require('../generic/login'),
+
+  constants: {
+    system: {
+      hardware: {
+        manufacturer: 'TP-Link',
+        model: 'TL-SG105PE'
+      },
+      keychain: {
+        username: 'admin',
+        password: 'admin'
+      },
+      ipv4: {
+        address: '',
+        port: 80
+      }
+    },
+    network: {
+      physical: {
+        ports: {
+          nr: {
+            total: 5,
+            '1G': 5
+          }
+        }
+      }
+    }
+  },
+
+  read: {
+    $1: require('../tl-sg108e/read/name+ip'),
+    $2: require('../tl-sg108e/read/physical'),
+    $3: require('../tl-sg108e/read/igmp'),
+    $4: require('../tl-sg108e/read/vlan'),
+    $5: require('../tl-sg108e/read/lag'),
+    $statistics: require('../tl-sg108e/read/statistics'),
+    $6: require('../tl-sg108e/read/mirror')
+  },
+  write: {
+    $1: require('../tl-sg108e/write/name'),
+    $2: require('../tl-sg108e/write/keychain'),
+    $3: require('../tl-sg108e/write/igmp'),
+    $4: require('../tl-sg108e/write/vlan'),
+    $5: require('../tl-sg108e/write/lag'),
+    $6: require('../tl-sg108e/write/mirror'),
+    $7: require('../tl-sg108e/write/ip')
+  },
+  commit: require('../tl-sg108e/write/commit')
+
+};
+
+module.exports = Device;
