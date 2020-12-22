ca4e3a4ee4318cc0ce1aa82df6f5f2f3b0b075f0
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-12-21 16:26:30 -0800

    Cleanup

--- discovery/arp.js
+++ discovery/arp.js
@@ -1,8 +1,8 @@
 const EventEmitter = require('events');
 const LocalDevices = require('local-devices');
 
-const DEFAULT_SEARCH_INTERVAL = 600; // 10 minutes
-const DEFAULT_WAIT_INTERVAL = 10; // 10 seconds
+const DEFAULT_SEARCH_INTERVAL = 10 * 60 * 1000; // 10 minutes
+const DEFAULT_WAIT_INTERVAL = 10 * 1000; // 10 seconds
 
 class Arp extends EventEmitter {
 
@@ -17,13 +17,13 @@ class Arp extends EventEmitter {
       const run = async () => {
         let update = false;
         update |= this._update(await LocalDevices());
-        await new Promise(resolve => setTimeout(resolve, DEFAULT_WAIT_INTERVAL * 1000));
+        await new Promise(resolve => setTimeout(resolve, DEFAULT_WAIT_INTERVAL));
         update |= this._update(await LocalDevices());
         if (update) {
           this.emit('update');
         }
       };
-      this.discovery = setInterval(run, DEFAULT_SEARCH_INTERVAL * 1000);
+      this.discovery = setInterval(run, DEFAULT_SEARCH_INTERVAL);
       run();
     }
   }
@@ -31,8 +31,9 @@ class Arp extends EventEmitter {
   _update(devices) {
     let update = false;
     devices.forEach(device => {
-      if (!this.found[device.ip] || this.found[device.ip].txt.macAddress != device.mac || this.found[device.ip].txt.hostname != device.name) {
-        this.found[device.ip] = { type: 'arp', ip: device.ip, txt: { macAddress: device.mac, hostname: device.name } };
+      const name = device.name === '?' ? '' : device.name;
+      if (!this.found[device.ip] || this.found[device.ip].txt.macAddress != device.mac || this.found[device.ip].txt.hostname != name) {
+        this.found[device.ip] = { type: 'arp', ip: device.ip, txt: { macAddress: device.mac, hostname: name } };
         update = true;
       }
     });
