dbfacfaae0be1bf1e1da6d24c287d9195b3edcc4
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-12-25 18:54:37 -0800

    Missing guard

--- device/switch/trendnet/teg-30284/write/igmp.js
+++ device/switch/trendnet/teg-30284/write/igmp.js
@@ -2,11 +2,14 @@ module.exports = {
   network: {
     igmp: {
       snoop: {
-        $: 'oid+set',
-        arg: '1.3.6.1.4.1.28866.3.1.11.2.1.1.0',
-        map: {
-          false: 2,
-          true: 1
+        $: 'guard',
+        arg: {
+          $: 'oid+set',
+          arg: '1.3.6.1.4.1.28866.3.1.11.2.1.1.0',
+          map: {
+            false: 2,
+            true: 1
+          }
         }
       }
     }
