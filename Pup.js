f48f0b5ecabce6dc3b42bb64b6ecd6d8478b8acc
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-03-08 22:07:42 -0800

    Attempted performance improvements

--- Pup.js
+++ Pup.js
@@ -23,7 +23,8 @@ class Pup {
           '--in-process-gpu',
           '--disable-dev-shm-usage',
           //'--proxy-server="direct://"',
-          //'--proxy-bypass-list=*'
+          //'--proxy-bypass-list=*',
+          '--user-data-dir=/tmp/pup'
         ],
         dumpio: true
       };
@@ -40,21 +41,24 @@ class Pup {
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37');
     await page.setViewport({ width: 1920, height: 1080 });
     await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-us' });
-    if (Log.enabled) {
+    if (Log.enabled || true) {
       /*
       * NOTE: Enabling this code makes page loads significantly slower
+      * Mar 9, 2021 - re-enabling with verison 8.0.0 to see if things have improved.
       */
       await page.setRequestInterception(true);
       page.on('request', req => {
         const type = req.resourceType();
-        Log('request:', req.url(), type);
-        if (type === 'image') {
+        if (type === 'image' || type === 'stylesheet' || type === 'font') {
+          Log('request aborted:', req.url(), type);
           req.abort();
         }
         else {
           req.continue();
         }
-    });
+      });
+    }
+    if (Log.enabled) {
       page.on('pageerror', err => Log('page error:', err.toString()));
       page.on('error', err => Log('error:', err.toString()));
       page.on('console', msg => Log('console:', msg.text()));
@@ -63,9 +67,14 @@ class Pup {
       page.on('domcontentloaded', () => Log('domcontentloaded'));
       page.on('load', () => Log('load'));
       page.on('response', async res => {
-        const data = await res.buffer();
-        Log('response:', res.url());
-        Log(`  body: ${data.toString()}`);
+        try {
+          const data = await res.buffer();
+          Log('response:', res.url());
+          Log(`  body: ${data.toString()}`);
+        }
+        catch (_) {
+          // Ignore errors caused by aborted requests
+        }
       });
     }
 
